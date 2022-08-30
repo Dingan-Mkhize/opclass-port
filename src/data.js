292d04f2e3f2dfad20bebaaf7d9155295c6b5f14
Dingan Mkhize <dingan.mkhize@yahoo.com>
2022-08-30 08:21:11 +0100

    data.js re-added

--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
-//import HTML from "./assets/html.png";
-//import CSS from "./assets/css.png";
-//import JavaScript from "./assets/javascript.png";
-//import ReactImg from "./assets/react.png";
-//import GitHub from "./assets/github.png";
-//import Node from "./assets/node.png";
-//import Tailwind from "./assets/tailwind.png";
-//import Mongo from "./assets/mongo.png";
-//import Calculator from "./assets/jscal.png";
-//import Portfolio from "./assets/portfolioapp.png";
-//import BookList from "./assets/booklist.png";
+import HTML from "./assets/html.png";
+import CSS from "./assets/css.png";
+import JavaScript from "./assets/javascript.png";
+import ReactImg from "./assets/react.png";
+import GitHub from "./assets/github.png";
+import Node from "./assets/node.png";
+import Tailwind from "./assets/tailwind.png";
+import Mongo from "./assets/mongo.png";
+import Calculator from "./assets/jscal.png";
+import Portfolio from "./assets/portfolioapp.png";
+import BookList from "./assets/booklist.png";
 
-/*
+
 export const Stacks = [
   {
     title: "HTML",
@@ -66,8 +66,8 @@ export const Projects = [
     picture: BookList,
   },
 ];
-*/
 
+/*
 export const Stacks = [
   {
     title: "HTML",
@@ -123,3 +123,4 @@ export const Projects = [
     picture: "./assets/booklist.png",
   },
 ];
+*/
